3df0eb39e4d78610604bfcc0821cb95264ccd3e3
Thomas Li <thomasli1234567890@gmail.com>
2019-04-24 19:18:21 -0700

    Add all fxml

--- automation/src/automation/FXGUI.java
+++ automation/src/automation/FXGUI.java
@@ -3,8 +3,10 @@ package automation;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
@@ -28,13 +30,13 @@ public class FXGUI extends Application {
 	public void start(Stage primaryStage) throws IOException {
 		this.primaryStage= primaryStage;
 		this.primaryStage.setTitle("Auto Routine");
-		initGUI();
+		initLoginGUI();
 	}
-	public void initGUI() {
+	public void initLoginGUI() {
 		try {
 	        // Load root layout from fxml file.
 	        FXMLLoader loader = new FXMLLoader();
-	        loader.setLocation(FXGUI.class.getResource("FxGUIrecord.fxml"));
+	        loader.setLocation(FXGUI.class.getResource("FxGUIlogin.fxml"));
 	        rootLayout = (AnchorPane) loader.load();
 	        
 	        // Show the scene containing the root layout.
@@ -44,8 +46,28 @@ public class FXGUI extends Application {
 	    } catch (IOException e) {
 	        e.printStackTrace();
 	    }
+	}
+	@FXML 
+	private void mainGUI(ActionEvent event) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(FXGUI.class.getResource("FxGUImain.fxml"));
+			AnchorPane mainLayout = (AnchorPane) loader.load();
+			Scene scene = new Scene(mainLayout);
+			primaryStage.setScene(scene);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
+	}
+	@FXML
+	private void newuserGUI(ActionEvent Event) {
+		try {
+			Parent pane = FXMLLoader.load(getClass().getResource("FxGUInewuser.fxml"));
+			primaryStage.getScene().setRoot(pane);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) {
 		launch(args);
