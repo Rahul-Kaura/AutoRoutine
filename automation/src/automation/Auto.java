f64deb2417ffd230c36e4d7049dc33d2100f45d5
Thomas Li <thomasli1234567890@gmail.com>
2019-04-26 17:20:34 -0700

    Foundation work for Auto.java

--- automation/src/automation/Auto.java
+++ automation/src/automation/Auto.java
@@ -1,20 +1,43 @@
 package automation;
 import java.awt.AWTException;
-import java.awt.Dimension;
+import java.awt.MouseInfo;
+import java.awt.Point;
 import java.awt.Robot;
-import java.awt.Toolkit;
-import java.io.*;
 public class Auto {
-	// Incomplete Auto class that is able to automate tasks
-	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	int screenWidth = screenSize.width;
-	int screenHeight = screenSize.height;
+	// Incomplete Auto class that should be able to automate tasks
+	// Currently working on implementing Key and Mouse Listeners using System Hook
+	
 	public void roboAuto() throws AWTException {
 		//This is supposed to open up a txt containing the preprogrammed routines and do it
 		Robot rb = new Robot();
-		rb.mouseMove(screenWidth/2, screenHeight/2); //moves the mouse to the middle of the screen
+		
 	}
-	public void learnRoutine() {
+//	public void learnRoutine() {
 		//This tracks the mouse and keyboard actions and writes to a txt file 
+	//}
+//	public static FFmpegFrameRecorder getRecorder() throws Exception
+	//{
+		// if(recorder!=null)
+		 //{
+			// return recorder;
+		 //}
+		 //recorder = new FFmpegFrameRecorder(outputVideo,videoWidth,videoHeight);
+		 //recorder.setFrameRate(videoFrameRate);
+		 //recorder.setVideoCodecName("mpeg4");
+         //recorder.setVideoBitrate(videoBitRate);
+         //recorder.setFormat("mp4");
+         //recorder.setVideoQuality(videoQuality);; 
+         //recorder.start();
+         //return recorder;
+	//}
+	private static Point getMouseLocation() {
+		//mouse tracker
+		return MouseInfo.getPointerInfo().getLocation();
+	}
+	public static void main (String[] args) throws InterruptedException {
+		for (int i = 0;i<100;i++) {
+			System.out.println(getMouseLocation());
+			Thread.sleep(1000);
+		}
 	}
 }
