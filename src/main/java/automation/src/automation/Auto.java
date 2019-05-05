e7d75365fa04e49e39643c32177934fc4800a45d
Thomas Li <thomasli1234567890@gmail.com>
2019-05-04 18:13:32 -0700

    switch to jnativehook

--- src/main/java/automation/src/automation/Auto.java
+++ src/main/java/automation/src/automation/Auto.java
@@ -1,22 +1,27 @@
 package automation;
 import java.awt.AWTException;
-import java.awt.MouseInfo;
-import java.awt.Point;
+import java.awt.Dimension;
+import java.awt.Rectangle;
 import java.awt.Robot;
+import java.awt.Toolkit;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
+import listener.KeyListener;
+import listener.MouseListener;
 
-import lc.kra.system.keyboard.GlobalKeyboardHook;
-import lc.kra.system.keyboard.event.GlobalKeyAdapter;
-import lc.kra.system.keyboard.event.GlobalKeyEvent;
-import lc.kra.system.mouse.GlobalMouseHook;
-import lc.kra.system.mouse.event.GlobalMouseAdapter;
-import lc.kra.system.mouse.event.GlobalMouseEvent;
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
 public class Auto {
 	// Incomplete Auto class that should be able to automate tasks
-	// Currently working on implementing Key and Mouse Listeners using System Hook
-	
+	// Currently working on implementing Key and Mouse Listeners using jnativehook
+	private int screenHeight;
+	private int screenWidth;
+	private boolean startCapture = false;
+	public Auto() {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		screenHeight = screenSize.height;
+		screenWidth = screenSize.width;
+	}
 	public void roboAuto(String routinename) throws AWTException, FileNotFoundException {
 		//This is supposed to open up a txt containing the preprogrammed routines and do it
 		Robot rb = new Robot();
@@ -24,60 +29,29 @@ public class Auto {
 		
 		
 	}
-//	public void learnRoutine() {
+	public void learnRoutine() {
 		//This tracks the mouse and keyboard actions and writes to a txt file 
-	//}
-	private static void getMouseLocation() {
-		//mouse tracker
-		GlobalMouseHook mouseHook = new GlobalMouseHook();
-		GlobalMouseAdapter adapter = new GlobalMouseAdapter() {
-			public ArrayList<GlobalMouseEvent> coordslist;
-			private boolean run=true;
-			@Override 
-			public void mousePressed(GlobalMouseEvent event)  {
-				coordslist.add(event);
-			}
-			
-			@Override 
-			public void mouseReleased(GlobalMouseEvent event)  {
-				coordslist.add(event); 
-			}
-			
-			@Override 
-			public void mouseMoved(GlobalMouseEvent event) {
-				coordslist.add(event); 
-			}
-			
-			@Override 
-			public void mouseWheel(GlobalMouseEvent event) {
-				coordslist.add(event); 
-			}
-			public ArrayList getContent() {
-				return coordslist;
-			}
-		};
-		mouseHook.addMouseListener(adapter);
-		//TODO Fix stupid bug here
-		//return adapter.coordslist;
-	}
-	private static void getkeyPressed() {
-		GlobalKeyboardHook keyboardHook = new GlobalKeyboardHook(true);
-		keyboardHook.addKeyListener(new GlobalKeyAdapter() {
-			private boolean run=true;
-			private ArrayList<GlobalKeyEvent> keylist;
-			@Override 
-			public void keyPressed(GlobalKeyEvent event) {
-				if (event.getVirtualKeyCode() == GlobalKeyEvent.VK_ESCAPE) {
-					run = false;
-				}
-				keylist.add(event);
-			}
-			
-			@Override 
-			public void keyReleased(GlobalKeyEvent event) {
-				keylist.add(event);
-			}
-		});
+		addListeners();
 		
+		
+	}
+	private static void addListeners() {
+		try {
+			GlobalScreen.registerNativeHook();
+		} catch (NativeHookException e) {
+			e.printStackTrace();
+		}
+		KeyListener keylistener = new KeyListener();
+		MouseListener mouselistener = new MouseListener();
+		GlobalScreen.addNativeKeyListener(keylistener);
+		GlobalScreen.addNativeMouseListener(mouselistener);
+	}
+	private void takeScreenshot(Rectangle screenRect) {
+		try {
+			Robot rb = new Robot();
+			rb.createScreenCapture(screenRect);
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
 	}
 }
