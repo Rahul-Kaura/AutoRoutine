a6305690fd593bfea31ecdb40e83094940f9bac5
Thomas Li <thomasli1234567890@gmail.com>
2019-05-04 18:14:18 -0700

    Add listener class

---
+++ src/main/java/automation/src/listener/KeyListener.java
@@ -0,0 +1,27 @@
+package listener;
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+public class KeyListener implements NativeKeyListener {
+	public void nativeKeyPressed(NativeKeyEvent e) {
+		System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+
+		if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+			try {
+				GlobalScreen.unregisterNativeHook();
+			} catch (NativeHookException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	public void nativeKeyReleased(NativeKeyEvent e) {
+		System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+	}
+
+	public void nativeKeyTyped(NativeKeyEvent e) {
+		System.out.println("Key Typed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+	}
+
+}
---
+++ src/main/java/automation/src/listener/MouseListener.java
@@ -0,0 +1,24 @@
+package listener;
+import org.jnativehook.mouse.NativeMouseEvent;
+import org.jnativehook.mouse.NativeMouseInputListener;
+public class MouseListener implements NativeMouseInputListener {
+	public void nativeMouseClicked(NativeMouseEvent e) {
+		System.out.println("Mouse Clicked: " + e.getClickCount());
+	}
+
+	public void nativeMousePressed(NativeMouseEvent e) {
+		System.out.println("Mouse Pressed: " + e.getButton());
+	}
+
+	public void nativeMouseReleased(NativeMouseEvent e) {
+		System.out.println("Mouse Released: " + e.getButton());
+	}
+
+	public void nativeMouseMoved(NativeMouseEvent e) {
+		System.out.println("Mouse Moved: " + e.getX() + ", " + e.getY());
+	}
+
+	public void nativeMouseDragged(NativeMouseEvent e) {
+		System.out.println("Mouse Dragged: " + e.getX() + ", " + e.getY());
+	}
+}
