a55e2d154803fc1d3703458ada8a172320508924
Thomas Li <thomasli1234567890@gmail.com>
2019-05-06 17:47:15 -0700

    improved keylistener

--- automation/src/main/java/listener/KeyListener.java
+++ automation/src/main/java/listener/KeyListener.java
@@ -1,27 +1,21 @@
 package main.java.listener;
-import org.jnativehook.GlobalScreen;
-import org.jnativehook.NativeHookException;
+import java.util.ArrayList;
 import org.jnativehook.keyboard.NativeKeyEvent;
 import org.jnativehook.keyboard.NativeKeyListener;
 public class KeyListener implements NativeKeyListener {
+	private ArrayList<Integer> keys;
 	public void nativeKeyPressed(NativeKeyEvent e) {
-		System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
-
-		if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
-			try {
-				GlobalScreen.unregisterNativeHook();
-			} catch (NativeHookException e1) {
-				e1.printStackTrace();
-			}
-		}
+		keys.add(e.getKeyCode());
 	}
 
 	public void nativeKeyReleased(NativeKeyEvent e) {
-		System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+		//System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
 	}
 
 	public void nativeKeyTyped(NativeKeyEvent e) {
-		System.out.println("Key Typed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+		//System.out.println("Key Typed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
 	}
-
+	public ArrayList<Integer> returnKeys() {
+		return keys;
+	}
 }
